d3ffe98fa065b2588e468355b4160615f7b244b5
rwsargent <rw.sarge@gmail.com>
2015-10-27 14:53:14 -0600

    added a set and participant model outline

--- scripts/start.js
+++ scripts/start.js
@@ -4,8 +4,8 @@ module.exports = function(username, api_key, t_name) {
     var tournaments = challonge.tournament.index();
     var test8 = null;
     for(var i = 0; i < tournaments.length; i++) { 
-	if (tournaments[i].tournament["name"] === t_name) { 
-	    test8 = tournaments[i];
-	}
+	    if (tournaments[i].tournament["name"] === t_name) { 
+	        test8 = tournaments[i];
+	    }
     }
 };

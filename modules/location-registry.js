07195a030626744016426fd06ecc5842694d1bdc
rwsargent <rw.sarge@gmail.com>
2015-11-02 11:23:38 -0700

    finalized location-registry

--- modules/location-registry.js
+++ modules/location-registry.js
@@ -2,43 +2,64 @@ var self = this;
 var tournamentToLocation = {};
 var locationToTournament = {};
 function addToMap(map, key, value) {
-    if(map[key]) {
-	map[key] = [];
+    if(!map[key]) {
+	    map[key] = [];
     }
     map[key].push(value);
 }
 function isEmpty(obj) {
     for(prop in obj) {
-	return true;
+	    return false;
+    }
+    return true;
+}
+
+function inArray(arr, obj) {
+    for(arrIdx in arr) {
+        if (arr[arrIdx] === obj) {
+            return true;
+        }
     }
     return false;
 }
-module.exports = {
-    init : function() {
-	var config = require('../server-config.json');
-	if(isEmpty(tournamentToLocation) && isEmpty(locationToTournament)) {
-	    
-	}
-    },
-    addLocationToTournament : function(tournamentName, location) {
+
+var addLocationToTournament = function(tournamentName, location) {
 	addToMap(tournamentToLocation, tournamentName, location);
 	addToMap(locationToTournament, location, tournamentName);
+};
+
+module.exports = {
+    init : function() {
+	    var config = require('../server-config.json');
+	    if(isEmpty(tournamentToLocation) && isEmpty(locationToTournament)) {
+	        for(tIdx in config.tournaments) {
+                var tournament = config.tournaments[tIdx];
+                for(lIdx in tournament.locations) {
+                    var location = tournament.locations[lIdx];
+                    if (inArray(config.locations, location)) {
+                        addLocationToTournament(tournament.name, location);
+                    } else {
+                        throw("Broken config, tournament has a location that doesn't exist");
+                    }
+                }
+            }
+	    }
     },
     getTournamentsForLocations : function(location) {
-	return locationToTournament[location];
+	    return locationToTournament[location];
     }, 
     getLocationsForTournament : function(tournamentName) {
-	return tournamentToLocation[tournamentName];
+	    return tournamentToLocation[tournamentName];
     },
     isValidLocationForTournament : function(tournamentName, location) {
-	var associatedTournaments = locationToTournament[location];
-	if (associatedTournaments) {
-	    for(aIdx in associatedTournaments) {
-		if(associatedTournaments[aIdx] === tournamentName) {
-		    return true;
-		}
+	    var associatedTournaments = locationToTournament[location];
+	    if(associatedTournaments) {
+	        for(aIdx in associatedTournaments) {
+		        if(associatedTournaments[aIdx] === tournamentName) {
+		            return true;
+		        }
+	        }
 	    }
-	}
-	return false;
+	    return false;
     }
 }

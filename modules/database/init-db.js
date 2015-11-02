2751e1653da8b621182c3d35a55a465d932d5cb8
rwsargent <rw.sarge@gmail.com>
2015-11-02 11:24:52 -0700

    changed whitespace for init-db

--- modules/database/init-db.js
+++ modules/database/init-db.js
@@ -10,7 +10,7 @@ var handleError = function(err) {
 
 var handleSave = function(err) {
     if(err) {
-	handleError(err);    
+	    handleError(err);    
     }
 }
 
@@ -27,77 +27,77 @@ module.exports = function() {
     };
 
     var loadIfNotPresent = function(model, loadFunction) {
-	model.count({}, function(err, count) {
-	    if(err) {
-		handleError(err);
-	    } else if (count) {
-		console.log(model.collection + " is already populated.");
-	    } else {
-		loadFunction();
-	    }
-	});
+	    model.count({}, function(err, count) {
+	        if(err) {
+		        handleError(err);
+	        } else if (count) {
+		        console.log(model.collection + " is already populated.");
+	        } else {
+		        loadFunction();
+	        }
+	    });
     }
 
     self.loadLocationsIntoDatabase = function() {
-	var locationRegistry = require('../location-registry.js');
-	for(lIdx in serverState.config.locations) {
-	    var location = serverState.config.locations[lIdx];
-	    var tourneys = locationRegistry.getTournamentsForLocations(location);
+	    var locationRegistry = require('../location-registry.js');
+	    for(lIdx in serverState.config.locations) {
+	        var location = serverState.config.locations[lIdx];
+	        var tourneys = locationRegistry.getTournamentsForLocations(location);
             var locationObject = Location({
                 name: location,
                 currentMatch : -1,
                 tournaments : tourneys,
             });
             locationObject.save(handleSave);
-	}
+	    }
     }
     
     self.loadMatchesIntoDatabase = function() {
-	for (tourneyIdx in serverState.config.tournaments) {
-	    var tournamentName = serverState.config.tournaments[tourneyIdx].name;
-	    var matches = challonge.matches.index(tournamentName);
-	    for (matchIdx in matches) {
-		var match = matches[matchIdx].match;
-		var matchObject = Match( {
-		    tournamentName : tournamentName,
-		    challongeMatchId : match.id,
-		    player1Challonge : match.player1_id,
-		    player2Challonge : match.player2_id,
-		    challongeIdentifier : match.identifier,
-		    round : match.round,
-		    state : match.state,
-		    winnerId : match.winner_id,
-		    loserId : match.loser_id,
-		    prereqMatchIds : match.prerequisite_match_ids_csv
-		});
-		matchObject.save(handleSave);
+	    for (tourneyIdx in serverState.config.tournaments) {
+	        var tournamentName = serverState.config.tournaments[tourneyIdx].name;
+	        var matches = challonge.matches.index(tournamentName);
+	        for (matchIdx in matches) {
+		        var match = matches[matchIdx].match;
+		        var matchObject = Match( {
+		            tournamentName : tournamentName,
+		            challongeMatchId : match.id,
+		            player1Challonge : match.player1_id,
+		            player2Challonge : match.player2_id,
+		            challongeIdentifier : match.identifier,
+		            round : match.round,
+		            state : match.state,
+		            winnerId : match.winner_id,
+		            loserId : match.loser_id,
+		            prereqMatchIds : match.prerequisite_match_ids_csv
+		        });
+		        matchObject.save(handleSave);
+	        }
 	    }
-	}
     };
 
     self.loadParticipants = function() {
-	for(tourneyIdx in serverState.config.tournaments) {
-	    var tourney = serverState.config.tournaments[tourneyIdx];
-	    self.participants = challonge.participants.index(tourney.name); // self as member variable
-	    if (!self.participants.length) {
-		console.log(tourney.name + " doesn't have any participants yet!");
+	    for(tourneyIdx in serverState.config.tournaments) {
+	        var tourney = serverState.config.tournaments[tourneyIdx];
+	        self.participants = challonge.participants.index(tourney.name); // self as member variable
+	        if (!self.participants.length) {
+		        console.log(tourney.name + " doesn't have any participants yet!");
+	        }
+	        for(partIdx in self.participants) {
+		        var participant = self.participants[partIdx].participant;
+		        var participantObject = Participant({
+		            challongeId : participant.id,
+		            name : participant.name,
+		            tournamentName : tourney.name
+		        });
+		        participantObject.save(handleSave);
+	        }
 	    }
-	    for(partIdx in self.participants) {
-		var participant = self.participants[partIdx].participant;
-		var participantObject = Participant({
-		    challongeId : participant.id,
-		    name : participant.name,
-		    tournamentName : tourney.name
-		});
-		participantObject.save(handleSave);
-	    }
-	}
     }
 
     self.init = function() {
-	loadIfNotPresent(Participant, self.loadParticipants);
-	loadIfNotPresent(Match, self.loadMatchesIntoDatabase);
-	loadIfNotPresent(Location, self.loadLocationsIntoDatabase);
+	    loadIfNotPresent(Participant, self.loadParticipants);
+	    loadIfNotPresent(Match, self.loadMatchesIntoDatabase);
+	    loadIfNotPresent(Location, self.loadLocationsIntoDatabase);
     };
 
     self.init();
